8379e5e032f9a113788f958ab6e13038fd035d42
rafalcode <ramonfallon@gmail.com>
2019-12-22 10:16:15 +0000

    latest

--- Makefile
+++ Makefile
@@ -14,6 +14,9 @@ ex0v: ex0v.cpp
 	${CC} ${CFLAGS} $(INCS) -o $@ $^ $(SPECLIBS)
 sineosc: sineosc.cpp
 	${CC} ${CFLAGS} $(INCS) -o $@ $^ $(SPECLIBS)
+# where as rawosc, throws caution to wind and grabs any raw you give it (1 arg)
+rawosc: rawosc.cpp
+	${CC} ${CFLAGS} $(INCS) -o $@ $^ $(SPECLIBS)
 
 .PHONY: clean
 
--- sineosc.cpp
+++ sineosc.cpp
@@ -1,11 +1,5 @@
 // sineosc.cpp STK tutorial program
 
-/* now this guy relies on a raw file,
- * so pretty much it just wavifies it .. not so great!
- *
- * That's what I said in the beginning, but the commentary on this tutorial
- * verifies that the raw file is actually the table. */
-
 #include "FileLoop.h"
 #include "FileWvOut.h"
 #include <cstdlib>
@@ -14,47 +8,46 @@ using namespace stk;
 
 int main()
 {
-    // Set the global sample rate before creating class instances.
-    Stk::setSampleRate( 44100.0 );
+  // Set the global sample rate before creating class instances.
+  Stk::setSampleRate( 44100.0 );
 
-    // int numfreqs=3;
-    // double freqa[numfreqs]={180., 440.0, 1400.};
+  int nFrames = 100000;
+  FileLoop input;
+  FileWvOut output;
 
-    int nFrames = 100000;
-    FileLoop input;
-    FileWvOut output;
+  try {
+    // Load the sine wave file.
+    input.openFile( "rawwaves/sinewave.raw", true );
 
+    // Open a 16-bit, one-channel WAV formatted output file
+    output.openFile( "hellosine.wav", 1, FileWrite::FILE_WAV, Stk::STK_SINT16 );
+  }
+  catch ( StkError & ) {
+    exit( 1 );
+  }
+
+  input.setFrequency( 440.0 );
+
+  // Option 1: Use StkFrames
+  /*
+  StkFrames frames( nFrames, 1 );
+  try {
+    output.tick( input.tick( frames ) );
+  }
+  catch ( StkError & ) {
+    exit( 1 );
+  }
+  */
+
+  // Option 2: Single-sample computations
+  for ( int i=0; i<nFrames; i++ ) {
     try {
-        // Load the sine wavetable file. It's 2048 bytes worth of shorts 2 byte samples, mono.
-        input.openFile( "sinewave.raw", true );
-
-        // Open a 16-bit, one-channel WAV formatted output file
-        output.openFile( "outsine.wav", 1, FileWrite::FILE_WAV, Stk::STK_SINT16 );
-    } catch ( StkError & ) {
-        exit( 1 );
+      output.tick( input.tick() );
     }
-
-    // for(int i=0;i<numfreqs;++i) {
-    // input.setFrequency(freqa[i]);
-    input.setFrequency(220.);
-
-    // Option 1: Use StkFrames
-    StkFrames frames( nFrames, 1 );
-    try {
-        output.tick( input.tick( frames ) );
-    } catch ( StkError & ) {
-        exit( 1 );
+    catch ( StkError & ) {
+      exit( 1 );
     }
+  }
 
-    // Option 2: Single-sample computations
-    /* for ( int i=0; i<nFrames; i++ ) {
-       try {
-       output.tick( input.tick() );
-       } catch ( StkError & ) {
-       exit( 1 );
-       }
-       }
-       */
-
-    return 0;
+  return 0;
 }
